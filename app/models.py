ad8f452aa3a09d0f8fe4be60a1e2420f80d3b691
Emmybrown775 <elumezeemma@gmail.com>
2024-12-30 00:46:02 +0100

    fixed big integer again

--- app/models.py
+++ app/models.py
@@ -1,18 +1,16 @@
 import uuid
-
-from sqlalchemy import Integer, String, JSON, BigInteger
-from sqlalchemy.orm import Mapped, mapped_column
 from app.extensions import db
+from sqlalchemy.dialects.postgresql import UUID, BIGINT, JSON
+
 
 class Users(db.Model):
-    id: Mapped[str] = mapped_column(primary_key=True, default=lambda : str(uuid.uuid4()))
-    chat_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
-    state: Mapped[str] = mapped_column(nullable=True)
-    balance: Mapped[float] = mapped_column(nullable=False,default=0)
-    account_number: Mapped[str] = mapped_column(nullable=True)
-    bank_code: Mapped[str] = mapped_column(nullable=True)
-    account_name: Mapped[str] = mapped_column(nullable=True)
-    bank_name: Mapped[str] = mapped_column(nullable=True)
-    temp_details: Mapped[dict] = mapped_column(JSON, default=dict)
-
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    chat_id = db.Column(BIGINT, unique=True, nullable=False)  # Explicitly use BIGINT for large chat IDs
+    state = db.Column(db.String, nullable=True)
+    balance = db.Column(db.Float, nullable=False, default=0)
+    account_number = db.Column(db.String, nullable=True)
+    bank_code = db.Column(db.String, nullable=True)
+    account_name = db.Column(db.String, nullable=True)
+    bank_name = db.Column(db.String, nullable=True)
+    temp_details = db.Column(JSON, default=dict)  # Use JSON for storing mutable objects
 
